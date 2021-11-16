a77ec6dc7dd33e505aa1a12026fcd322327b4dca
NelRondon01 <71944026+NelRondon01@users.noreply.github.com>
2021-11-16 16:57:32 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,42 +3,44 @@ $(document).ready(function () {
         let fecha_eli = new Date(2021, 9, 14, 23, 20);
         let fecha = new Date();
 
-        let hora=0, dia=0, sem=0, mes=0, years=0, min=0, seg=0;
+        let vals = [0, 0, 0, 0, 0, 0, 0]
 
         diff = fecha - fecha_eli;
         let a = 1000*60*60*24*7*4.345238095238095*12;
+        
+        vals[0] = (diff-(diff%a))/a
+        vals[1] = ((diff%a)/a)*12
+        vals[2] = (vals[1]-Math.trunc(vals[1]))*30.416666666666664
+        vals[3] = (vals[2]-Math.trunc(vals[2]))*24
+        vals[4] = (vals[3]-Math.trunc(vals[3]))*60
+        vals[5] = (vals[4]-Math.trunc(vals[4]))*60
 
-        years = (diff-(diff%a))/a
+        vals[5] = Math.trunc(vals[5])
+        vals[4] = Math.trunc(vals[4])
+        vals[3] = Math.trunc(vals[3])
+        vals[2] = Math.trunc(vals[2])
+        vals[1] = Math.trunc(vals[1])
 
-        mes = ((diff%a)/a)*12
-        dia = (mes-Math.trunc(mes))*30.416666666666664
-        hora = (dia-Math.trunc(dia))*24
-        min = (hora-Math.trunc(hora))*60
-        seg = (min-Math.trunc(min))*60
+        for(i=0;i<vals.length;i++){
+            if(vals[i] < 10){
+                vals[i] = "0"+vals[i]
+            }
+        }
 
-        seg = Math.trunc(seg)
-        min = Math.trunc(min)
-        hora = Math.trunc(hora)
-        dia = Math.trunc(dia)
-        mes = Math.trunc(mes)
+        $("#year .vl").text(vals[0])
+        $("#mes .vl").text(vals[1])
+        $("#dia .vl").text(vals[2])
+        $("#hora .vl").text(vals[3])
+        $("#min .vl").text(vals[4])
+        $("#seg .vl").text(vals[5])
+    }, 500);
 
-        let fecha_res = years+" años, "+mes+" meses, "+dia+" dias <br> "+hora+" horas, "+min+" min, "+seg+" seg"
-
-        $("#fecha").html(fecha_res);
-    }, 1000);
+    anime({
+        targets: ".fecha, h1",
+        scale: [1.2, 1],
+        translateY: [100, 0],
+        opacity: [0, 1],
+        duration: 2000,
+        delay: 1000
+    })
 });
-
-//Un centisegundo equivale a 10 milisegundos
-//Un decisegundo equivale a 100 milisegundos
-//Un segundo equivale a 1000 milisegundos
-//Un minuto equivale a 60 000 milisegundos
-//Una hora equivale a 3 600 000 milisegundos
-//Un día equivale a 86 400 000 milisegundos
-//Una semana equivale a 604 800 000 milisegundos
-//Un mes equivale a 2 628 000 000 milisegundos
-//Un año equivale a 31 536 000 000 milisegundos
-//Un lustro equivale a 157 680 000 000 milisegundos
-//Una década equivale a 315 360 000 000 milisegundos
-//Un siglo equivale a 3 153 600 000 000 milisegundos
-//Un milenio equivale a 31 536 000 000 000 milisegundos
-// 1000*60*60*24*7*4.345238095238095*12
